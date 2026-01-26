5cee8d4fc7f39c0e064e3dc2cda604425966c310
Magic Gapud <magicgapud214@gmail.com>
2026-01-26 08:15:49 +0800

    implement gsap

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
   /*------------------lenis---------------*/
 
-//   const lenis = new Lenis({ smooth: true });
-//   lenis.on("scroll", ScrollTrigger.update);
-//   gsap.ticker.add((time) => {
-//     lenis.raf(time * 1000);
-//   });
+  const lenis = new Lenis({ smooth: true });
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time* 10000);
+  });
 
-  gsap.ticker.lagSmoothing(0);
+//   gsap.ticker.lagSmoothing(0);
 
   // let tl = gsap.timeline({
 
@@ -32,11 +32,59 @@ window.addEventListener("DOMContentLoaded", () => {
       start: 40 % top,
       end: "+=300px",
       scrub: 1.5,
-      markers: true,
+    //   markers: true,
     },
     opacity: 0,
     duration: .5,
     yPercent:10
   });
 
+
+document.querySelectorAll(".description").forEach((el)=>{
+    el.style.background = el.dataset.bg;
+    el.style.color = el.dataset.color;
+})
+
+const revealtl = gsap.timeline({
+    delay: 1,
+    scrollTrigger: {
+        trigger: "#aboutMe",
+        start: "top 25%",
+        end: "bottom 20%",
+        scrub: 2,
+        pin: true,
+        markers: true,
+    },
 });
+
+
+revealtl.to(".first", reveal())
+.to(".second", reveal())
+.to(".third", reveal())
+.to(".fourth", reveal())
+.to(".first", hide())
+.to(".second", hide())
+.to(".third", hide())
+.to(".fourth", hide());
+
+function reveal(){
+    return{
+        duration: 2,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease:"circ.out",
+    }
+}
+
+function hide(){
+    return{
+        duration: 2,
+        opacity: 0,
+        clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0 100%)",
+        ease:"circ.out",
+    }
+}
+
+
+});
+
